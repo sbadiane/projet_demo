9b5f3203d5f41cd6948678ff30f4ed6bf293c636
sbadiane <sbadiane@unikerp.com>
2024-05-23 13:45:31 +0000

    test

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "test_demo",
+    'version': '17.0.1.0.0',
+    'depends': ["base"],
+    "author":"Unikerp",
+    "website" : "www.unikerp.com",
+    'description': """ Gestion des demo """,
+    'application': True,
+    'module_type' : 'officiel',
+    'data': [ ],
+}
