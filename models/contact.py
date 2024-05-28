865b8e84b5f754bf82703a21d1d433057fc0444d
Ndeyefatu <nfndiaye@unikerp.com>
2024-05-28 10:54:40 +0000

    projetdemo

--- models/contact.py
+++ models/contact.py
@@ -3,17 +3,3 @@ class Contacts(models.Model):
     _inherit = "res.partner"
    
     
-    @api.model
-    def create(self,vals):
-     print('tttttttttttttttttttttttttttttt')
-     results = super(Contacts, self).create(vals)
-     print("pppppppppppppppppp",self.partner_id,self.email)
-           
-     self.env['res.users'].create({
-                     
-                    "name":self.partner_id,
-                    "login": self.email,
-                    
-                            
-            })
-     return results
